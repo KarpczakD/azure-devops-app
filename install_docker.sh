c1df68b3b49b4f7ea6455787b78e2099d784cdbc
DawidK <karpczak@gmail.com>
2025-04-29 11:31:04 +0200

    Fix: use port 5000 instead of 80 in install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,4 +2,4 @@
 apt-get update
 apt-get install -y docker.io
 systemctl start docker
-docker run -d -p 80:5000 karpik3004/azure-devops-app:latest
+docker run -d -p 5000:5000 karpik3004/azure-devops-app
